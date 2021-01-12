19a955ea0fe3436ffab5dbc7fc4e81cae28e96d2
Maksim Artemov <martemov@deem.com>
2021-01-12 11:51:11 +0000

    Initial Commit

---
+++ ReactiveCalendarIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReactiveCalendarIOS
+//
+//  Created by Maksim Artemov on 12/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
