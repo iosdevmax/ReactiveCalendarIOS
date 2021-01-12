e26b16bcaf5eb42ca68310f62963a76c9329b91a
iosdevmax <max6361@mail.ru>
2021-01-12 17:00:58 +0000

    added main folder

---
+++ ReactiveCalendarIOS/Calendar/Calendar.swift
@@ -0,0 +1,17 @@
+//
+//  Calendar.swift
+//  ReactiveCalendarIOS
+//
+//  Created by Maksim Artemov on 12/01/2021.
+//
+
+import UIKit
+
+class ReactiveCalendar: UIView {
+    
+}
+
+class ReactiveCalendarViewModel {
+    
+}
+
--- ReactiveCalendarIOS/CalendarViewController.swift
+++ ReactiveCalendarIOS/CalendarViewController.swift
@@ -20,4 +20,3 @@ class CalendarViewController: UIViewController {
 
 
 }
-
